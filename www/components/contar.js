a55aca46282806d965c9ab98aae82879275f8238
Fernanch-03 <fefemachado03@gmail.com>
2020-02-28 03:42:26 +0900

    Terminei

--- www/components/contar.js
+++ www/components/contar.js
@@ -1,2 +1,18 @@
-// This is a JavaScript file
+$(document).on("click","#calcular",function(){
+var valor1 = $("#valor1").val();
+var valor2 = $("#valor2").val();
+var valor3 = $("#valor3").val();
 
+var result = (parseFloat(valor3)*parseFloat(valor2))/parseFloat(valor1);
+$("#valorx").val(result);
+
+});
+
+$(document).on("click","#limpar",function(){
+
+$("#valor1").val("");
+$("#valor2").val("");
+$("#valor3").val("");
+$("#valorx").val("X");
+
+});
